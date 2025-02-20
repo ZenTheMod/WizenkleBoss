6da02548a4363dc2d1a61d1caf7523f2841a9bd5
ZenTheMod <88523932+ZenTheMod@users.noreply.github.com>
2025-02-19 20:24:38 -0800

    i hate you

---
+++ Common/ILDetourSystems/DisableRetroLightingSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Graphics.Light;
+using Terraria.ModLoader;
+
+namespace WizenkleBoss.Common.ILDetourSystems
+{
+    public class DisableRetroLightingSystem : ModSystem
+    {
+        public override void Load()
+        {
+            On_Lighting.NextLightMode += LetsBeHonestHereYouWereNOTGainingANYPreformanceFromThisFuckYou;
+            if (!Lighting.NotRetro)
+                Lighting.Mode = LightMode.White;
+        }
+
+        public override void Unload()
+        {
+            On_Lighting.NextLightMode -= LetsBeHonestHereYouWereNOTGainingANYPreformanceFromThisFuckYou;
+        }
+
+        private void LetsBeHonestHereYouWereNOTGainingANYPreformanceFromThisFuckYou(On_Lighting.orig_NextLightMode orig)
+        {
+            if (!Lighting.NotRetro)
+                Lighting.Mode = LightMode.White;
+            orig();
+        }
+    }
+}
