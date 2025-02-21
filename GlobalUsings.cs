ee53fe0f11607a65d87b68cc6f0f69794db9c690
ZenTheMod <88523932+ZenTheMod@users.noreply.github.com>
2025-02-20 23:23:00 -0800

    Update GlobalUsings.cs

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,3 +1,4 @@
 ﻿    // fuck you
 global using Vector2 = Microsoft.Xna.Framework.Vector2;
 global using Color = Microsoft.Xna.Framework.Color;
+global using Rectangle = Microsoft.Xna.Framework.Rectangle;
